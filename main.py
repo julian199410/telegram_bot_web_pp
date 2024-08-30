7c274ae10c1d0c200257fb21cb96c6f2d0bfd433
julian <julian@entrenese.com>
2024-08-29 21:52:32 -0500

    [Fix] url validate.

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ async def read_root():
             <script async src="https://telegram.org/js/telegram-widget.js?22"
                 data-telegram-login="Comfabot"
                 data-size="large"
-                data-auth-url="/https://telegram-bot-web-pp.onrender.com/"
+                data-auth-url="/https://telegram-bot-web-pp.onrender.com/auth"
                 data-request-access="write">
             </script>
             <script type="text/javascript">
